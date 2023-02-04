f7c5d1f275c9e5c09273f349304489cb870d650f
Bella-devv <82674742+Bella-devv@users.noreply.github.com>
2023-02-03 22:20:07 -0300

    Add files via upload

---
+++ src/Dog/Cachorro.java
@@ -0,0 +1,24 @@
+package Dog;
+
+public class Cachorro {
+
+	String raca;
+	
+	
+	public void emitirSom() {
+		// TODO Auto-generated method stub
+		System.out.println("emitiu som");
+		
+	}
+
+
+	public void correr() {
+		// TODO Auto-generated method stub
+		System.out.println("correu");
+	}
+	}
+
+
+
+	
+
---
+++ src/Dog/Teste.java
@@ -0,0 +1,23 @@
+package Dog;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Cachorro d1 = new Cachorro	();
+          d1.raca = "viralata";
+          d1.emitirSom();
+          d1.correr();
+          
+          
+          
+     
+  
+
+		
+		
+		
+	}
+
+}
